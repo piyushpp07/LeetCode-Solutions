723e90b1897ce25ae1661d842ae3a42877143999
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2021-01-11 10:47:37 +0530

    Update equalPartition.cpp

--- equalPartition.cpp
+++ equalPartition.cpp
@@ -9,17 +9,16 @@
      return 0;
      else{
          sum=sum/2;
-     bool dp[N+1][sum+1];
+        bool dp[N+1][sum+1];   //initialise dp array to store
         for(int i=0; i<=sum; i++)
-        dp[0][i]=false;
+        dp[0][i]=false;            
         for(int i=0; i<=N; i++)
         dp[i][0]=true;  //with zero element we can make sum zero
         for(int i=1; i<=N; i++){
             for(int j=1; j<=sum; j++){
-                if(v[i]>j)
-                dp[i][j]=dp[i-1][j];
+                if(v[i]>j)  dp[i][j]=dp[i-1][j];
                 else dp[i][j]=dp[i-1][j-v[i]]||dp[i-1][j];
-            }
-        }
+                               }
+         }
          return dp[N][sum];
-    }}
+     }
