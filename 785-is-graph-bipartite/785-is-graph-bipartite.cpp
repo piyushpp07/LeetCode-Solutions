1c9df461f9aeda1acb1949c51aa79477fa6d969a
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2022-01-05 00:42:16 +0530

    Time: 71 ms (5.34%), Space: 18.1 MB (7.05%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,42 +1,39 @@
 class Solution {
-private:
-    bool isbp(int col[],vector<vector<int>>& graph,int i)
-    {
-        queue<int>q;
-        q.push(i);
-        col[i]=1;
-        while(q.size()>0)
-        {
-            int a=q.front();
-            q.pop();
-            for(int m:graph[a])
-            {
-                if(col[m]==-1)
-                {
-                    col[m]=1-col[a];
-                    q.push(m);
-                }
-                else if(col[m]==col[a])
-                {
+public:
+    bool checkBipartiteDfs(int node, vector<int> adj[], vector<int> &color){
+        if(color[node] == -1) color[node] =1;
+        for(auto x : adj[node]){
+            if(color[x] == -1){
+                color[x] = 1 - color[node];
+                if(!checkBipartiteDfs(x, adj, color)){
                     return false;
                 }
+            }
+            else if(color[x] == color[node]){
+                return false;
             }
         }
         return true;
     }
-public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int V=graph.size();
-        int col[V+1];
-        memset(col,-1,sizeof(col));
-        for(int i=0;i<V;i++)
-        {
-            if(col[i]==-1)
-            {
-                if(isbp(col,graph,i)==false)
+        int n = graph.size();
+        vector<int> adj[n];
+        vector<int> color(n, -1);
+        for(int i=0;i<n;i++){
+            for(auto x : graph[i]){
+                adj[i].push_back(x);
+                adj[x].push_back(i);
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(color[i] == -1){
+                if(!checkBipartiteDfs(i,adj,color)){
                     return false;
+                }
             }
         }
         return true;
+        
     }
 };
