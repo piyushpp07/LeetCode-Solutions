486db99a69f4964ed6f7979e133eae3e27d67476
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2022-01-06 15:07:25 +0530

    Time: 12 ms (76.75%), Space: 10.2 MB (60.37%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-    map<int,int>sortedmap;
-    int n=trips.size();
+        int n=trips.size();
+        vector<int>sortedmap(1001,0);
+
         for(int i=0;i<n;i++)
         {
             sortedmap[trips[i][1]]+=trips[i][0];
             sortedmap[trips[i][2]]-=trips[i][0];
         }
-        for(auto it = sortedmap.begin(); it != sortedmap.end();it++)
+        for(int i=0;i<1001;i++)
         {
-            capacity-=it->second;
+            capacity-=sortedmap[i];
             if(capacity<0)
                 return false;
         }
