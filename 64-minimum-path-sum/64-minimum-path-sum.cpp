d957dd65f2be4f3ac0b1a2af9af8e97abe80e443
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2022-01-12 16:54:44 +0530

    Time: 12 ms (43.34%), Space: 10 MB (48.91%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+int dp[201][201];
+private:
+    int helper(int i ,int j, int n ,int m ,vector<vector<int>>&arr )
+    {
+        if(i>n-1||j>m-1)
+            return 0;
+        
+        if(i==n-1&&j==m-1)
+            return dp[i][j]= arr[i][j];
+    
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        if(i==n-1)
+            return  dp[i][j] = helper(i,j+1,n,m,arr)+arr[i][j];
+        
+        if(j==m-1)
+            return  dp[i][j] = helper(i+1,j,n,m,arr)+arr[i][j];
+        
+        return dp[i][j]= min(helper(i+1,j,n,m,arr),helper(i,j+1,n,m,arr))+arr[i][j];
+    }
+public:
+    int minPathSum(vector<vector<int>>& arr) {
+        int n=arr.size();
+        memset(dp,-1,sizeof dp);
+        int m=arr[0].size();
+        return helper(0,0,n,m,arr);
+    }
+};
