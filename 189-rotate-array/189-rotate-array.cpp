6672c2caafcb2f9907b8b37a2fe8f2ea87b8fd98
educated_Fool <59327328+piyushpp07@users.noreply.github.com>
2022-01-14 12:38:30 +0530

    Time: 59 ms (10.36%), Space: 25 MB (51.07%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-      vector<int>z(nums.size(),0);
-        int n=nums.size();
-        for(int i=0;i<nums.size();i++)
+     void swap(vector<int>&nums,int start , int end )
+    {
+        int temp = nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+    }
+   void reverse(vector<int>&nums,int start , int end )
+    {
+        while(start<=end)
         {
-            z[(i+k)%n]=nums[i];
-
+            swap(nums,start,end);
+            start++;
+            end--;
         }
-        nums=z;
+    }
+    void rotate(vector<int>&nums, int k) {
+        int n = nums.size();
+        k=k%n;
+        if(k<0)
+            k=k+n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
     }
 };
