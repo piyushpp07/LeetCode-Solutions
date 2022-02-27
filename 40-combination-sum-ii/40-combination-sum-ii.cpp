9391dfda0410fd9e885d57274e6ebfabf5063784
educated_Fool <59327328+piyushpp07@users.noreply.github.com>
2022-02-27 19:20:31 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (45.87%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,17 +1,24 @@
 class Solution {
     public: 
-    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds) {
+    void findCombination(int idx, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds) {
+
         if(target==0) {
             ans.push_back(ds);
             return;
-        }        
-        for(int i = ind;i<arr.size();i++) {
-            if(i>ind && arr[i]==arr[i-1]) continue; 
-            if(arr[i]>target) break; 
-            ds.push_back(arr[i]);
-            findCombination(i+1, target - arr[i], arr, ans, ds); 
-            ds.pop_back(); 
         }
+        for(int  i = idx;i<arr.size();i++)
+        {   if(i>idx && arr[i]==arr[i-1]) continue; 
+            if(target>=arr[i])
+            {
+                ds.push_back(arr[i]);
+                findCombination(i+1,target-arr[i],arr,ans,ds);
+                ds.pop_back();
+            }
+            else{
+                break;
+            }
+        }
+    
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
