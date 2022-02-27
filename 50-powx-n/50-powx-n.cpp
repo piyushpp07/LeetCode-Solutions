a5289872fda4a9258a954df20bc8abbc689022b0
educated_Fool <59327328+piyushpp07@users.noreply.github.com>
2022-02-27 15:01:43 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.31%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,39 +1,36 @@
 class Solution {
+private:
+  double solve(double x, int n)
+  { if(n == 0)
+    return 1;
+    if (n == 1)
+      return x;
+    double ans = solve(x, n/2);
+    if (n % 2 == 0)
+      return ans * ans;
+    else return  x * ans * ans;
+  }
+
 public:
-    
-    double solve(double x, int n){
-           //Base Case
-            if(n == 0){
-                return 1;
-            }
-            if(n == 1){
-                return x;
-            }
-            
-            //Recursive Call
-            double ans = solve(x, n/2);
-            
-            //Even
-            if(n%2 == 0){
-                return ans * ans;
-            }
-            //Odd
-            else{
-                return x * ans * ans;
-            }
-        }
-    
-    double myPow(double x, int n) {
-        
-		if(n < 0){
-            return 1 / solve(x, n);
-        }
-        else{
-            return solve(x, n);
-        }
+  double myPow(double x, int n) {
+
+    if (n == 0)
+      return 1;
+    else {
+ 
+      if (n > 0)
+      {
+        return solve(x, n);
+      }
+      else {
+
+        return 1/solve(x, n);
+      }
+
     }
 
 
 
-  
+
+  }
 };
