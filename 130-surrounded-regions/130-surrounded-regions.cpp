721d3941061c74745f19aed1ec66060be0e07fa8
educated_Fool <59327328+piyushpp07@users.noreply.github.com>
2022-03-03 13:31:10 +0530

    Time: 16 ms (63.63%), Space: 9.9 MB (77.25%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -44,10 +44,11 @@ public:
     {
       for (int j = 0; j < m; j++)
       {
-        if (board[i][j] == 'O')
-          board[i][j] = 'X';  
         if (board[i][j] == '#')
           board[i][j] = 'O';
+        else if (board[i][j] == 'O')
+          board[i][j] = 'X';  
+
 
       }
     }
