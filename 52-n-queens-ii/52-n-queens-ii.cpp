2a8ba7ca3711f3408b51444fafbb09477800cab2
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-06-04 10:00:26 +0530

    Time: 4 ms (78.56%), Space: 6.7 MB (35.10%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,57 @@
+class Solution
+{
+    private:
+        vector<vector < string>> a;
+    bool isValid(int r, int c, int n, vector<string> &ans)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            if (ans[i][c] == 'Q')
+                return false;
+        }
+        for (int i = 0; i < c; i++)
+        {
+            if (ans[r][i] == 'Q')
+                return false;
+        }
+        int a = r, b = c;
+        while (a >= 0 && b >= 0)
+        {
+            if (ans[a][b] == 'Q')
+                return false;
+            a--, b--;
+        }
+        a = r, b = c;
+        while (a >= 0 && b < n)
+        {
+            if (ans[a][b] == 'Q')
+                return false;
+            a--, b++;
+        }
+        return true;
+    }
+    void nQueens(int r, int c, int n, vector<string> &ans)
+    {
+        if (r == n)
+        {
+            a.push_back(ans);
+            return;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (isValid(r, i, n, ans))
+            {
+                ans[r][i] = 'Q';
+                nQueens(r + 1, 0, n, ans);
+                ans[r][i] = '.';
+            }
+        }
+    }
+    public:
+        int totalNQueens(int n)
+        {
+            vector<string> ans(n, string(n, '.'));
+            nQueens(0, 0, n, ans);
+            return a.size();
+        }
+};
