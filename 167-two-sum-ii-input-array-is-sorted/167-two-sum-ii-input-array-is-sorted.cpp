32d8457af636f606c575cb2d67f69e8e2ff29202
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-06-09 12:06:17 +0530

    Time: 8 ms (96.97%), Space: 15.6 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,40 +1,19 @@
 class Solution
 {
-    int binary(vector<int> &numbers, int req)
-    {
-        int s = 0, e = numbers.size() - 1;
-        while (s <= e)
-        {
-            int mid = (s + e) / 2;
-            if (numbers[mid] == req)
-                return mid;
-            else if (numbers[mid] > req)
-            {
-                e = mid - 1;
-            }
-            else
-                s = mid + 1;
-        }
-        return -1;
-    }
     public:
         vector<int> twoSum(vector<int> &numbers, int target)
         {
             vector<int> v;
-            for (int i = 0; i < numbers.size(); i++)
+            int lo = 0 , hi = numbers.size()-1;
+            while(lo<=hi)
             {
-                int a = target - numbers[i];
-                int idx = binary(numbers, a);
-                if (idx == -1 || idx == i)
-                    continue;
-                else
-                {
-                    v.push_back(i + 1);
-                    v.push_back(idx + 1);
-                    break;
-                }
+                    int sum = numbers[lo]+numbers[hi];
+                    if(sum == target)
+                            return {lo+1,hi+1};
+                    else if(sum>target) hi--;
+                    else lo++;
             }
-            sort(v.begin(), v.end());
             return v;
+       
         }
 };
