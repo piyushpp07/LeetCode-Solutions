05ccf453e51f090fc20fc36f12e814fdd3153c14
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-06-10 18:21:55 +0530

    Time: 1755 ms (5.00%), Space: 264.1 MB (5.24%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,25 @@
-class Solution {
-public:
-	int lengthOfLongestSubstring(string s) {
-		int ans = 0;
-        if(s.size()==1)
-            return 1;
-        if(s.size()==0)
-            return 0;
-		for (int i = 0; i < s.size(); i++)
-		{	int sz = 1;
-			map<char, int>mp;
-            mp[s[i]]++;
-			for (int j = i + 1; j < s.size(); j++)
-			{
-				if (mp.find(s[j]) != mp.end())
-				{
-					break;
-				}
-				else {
-					sz++;
-					mp[s[j]]++;
-				}
-
-			}
-			if (ans < sz)
-				ans = sz;
-
-		}
-		return ans;
-	}
+class Solution
+{
+    public:
+        int lengthOfLongestSubstring(string s)
+        {
+            int ans = 0;
+            for (int i = 0; i < s.size(); i++)
+            {
+                int minans = 0;
+                map<char, int> mp;
+                for (int j = i; j < s.size(); j++)
+                {
+                    if (mp.find(s[j]) != mp.end())
+                        break;
+                    else
+                    {
+                        minans++;
+                        mp[s[j]]++;
+                    }
+                }
+                ans = max(ans, minans);
+            }
+            return ans;
+        }
 };
