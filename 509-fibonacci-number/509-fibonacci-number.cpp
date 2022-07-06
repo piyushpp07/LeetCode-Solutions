ba75d83678afe1d3cd03ec3d6db2146b6643740e
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-07-06 12:42:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,13 @@
-class Solution
-{
-    public:
-        int fib(int n)
+        vector<int> dp(31, -1);
+        class Solution
         {
-            if (n == 0 || n == 1) return n;
-            else return fib(n - 1) + fib(n - 2);
-        }
-};
+
+            public:
+                int fib(int n)
+                {
+                    if (n == 0 || n == 1) return n;
+                    if (dp[n] != -1)
+                        return dp[n];
+                    else return dp[n] = fib(n - 1) + fib(n - 2);
+                }
+        };
