334af782600b046c3ed8cbd56291cebfd4ef8cb4
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-07-16 11:36:15 +0530

    Time: 258 ms (23.92%), Space: 40 MB (70.58%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,46 +1,47 @@
 class Solution
-	{
+{
 
-	public:
-		int networkDelayTime(vector<vector < int>> &times, int n, int k)
-		{
-			vector<vector<pair<int, int>>> adj(n + 1);
-			for (int i = 0; i < times.size(); i++)
-				adj[times[i][0]].push_back({ times[i][1],
-				                             times[i][2]
-				                           });
-			using ppi = pair<int, int> ;
-
-			vector<int> d(n + 1, INT_MAX);
-
-			priority_queue<ppi, vector <ppi>, greater < ppi>> q;
-			q.push({ 0, k});
-
-			d[k] = 0;
-			d[0] = 0;
-			while (!q.empty())
-			{
-				int v = q.top().second;
-				int d_v = q.top().first;
-				q.pop();
-				if (d_v != d[v])
-					continue;
-				for (auto edge : adj[v])
-				{
-					int to = edge.first;
-					int len = edge.second;
-					if (d[to] > d[v] + len)
-					{
-						d[to] = d[v] + len;
-						q.push({ d[to], to});
-					}
-				}
-			}
-			int ans = INT_MIN;
-			for (int i = 0; i < d.size(); i++)
-				if (d[i] > ans) ans = d[i];
-			if (ans == INT_MAX)
-				return -1;
-			else return ans;
-		}
-	};
+    public:
+        int networkDelayTime(vector<vector < int>> &t, int n, int k)
+        {
+            vector<vector<pair<int, int>>> adj(n + 1);
+            for (int i = 0; i < t.size(); i++)
+            {
+                adj[t[i][0]].push_back({ t[i][1],
+                    t[i][2] });
+            }
+            vector<int> dis(n + 1, INT_MAX);
+            dis[k] = 0;
+            dis[0] = 0;
+            priority_queue<pair<int, int>, vector< pair<int, int>>, greater<pair<int, int>>> pq;
+            pq.push({ 0,
+                k });
+            while (!pq.empty())
+            {
+                int v = pq.top().second;
+                int d_v = pq.top().first;
+                pq.pop();
+                if (dis[v] != d_v)
+                    continue;
+                for (auto i: adj[v])
+                {
+                    int to = i.first;
+                    int d_to = i.second;
+                    if (dis[to] > dis[v] + d_to)
+                    {
+                        dis[to] = dis[v] + d_to;
+                        pq.push({ dis[to],
+                            to });
+                    }
+                }
+            }
+            int ans = INT_MIN;
+            for (int i = 0; i < dis.size(); i++)
+            {
+                if (dis[i] > ans) ans = dis[i];
+            }
+            if (ans == INT_MAX)
+                return -1;
+            return ans;
+        }
+};
