7cc682e3b8aa08a8f15b6d61e8127e2e51193fd7
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-08-04 16:12:06 +0530

    Time: 254 ms (6.11%), Space: 13.4 MB (15.93%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -23,8 +23,21 @@ class Solution
     int minDistance(string a, string b)
     {
         int n = a.size(), m = b.size();
-        memset(dp, -1, sizeof dp);
-        int ans = solve(n - 1, m - 1, a, b);
-        return ans;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 0;i<=n;i++)
+            dp[i][0] = i;
+        for(int j = 0;j<=m;j++)
+            dp[0][j] = j;
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+                if(a[i-1]==b[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else 
+                    dp[i][j] =  1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
