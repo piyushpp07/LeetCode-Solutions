ac7c706c3df6ba2b87f29c3f0f980cf8531f16ae
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-08-11 12:22:57 +0530

    Time: 192 ms (65.40%), Space: 37.8 MB (93.13%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,12 +1,14 @@
-bool comp(vector<int>&a,vector<int>&b)
-{
-    return a[0]<b[0];
-}
 class Solution
 {
     public:
         int par[100001];
+    int s[100001];
     int cc = 0;
+    void make(int i)
+    {
+        par[i] = i;
+        s[i] = 1;
+    }
     int find(int a)
     {
         if (a == par[a])
@@ -18,26 +20,29 @@ class Solution
     {
         int l = find(a);
         int m = find(b);
-        if(l!=m)
+        if (l!=m)
         {
-            par[l] = m;
+            if (s[l] < s[m])
+                swap(l, m);
+            par[m] = l;
+            s[l] += s[m];
             cc++;
         }
     }
     int makeConnected(int n, vector<vector < int>> &connections)
     {
-        memset(par, 0, sizeof par); 
+        memset(par, 0, sizeof par);
+        memset(s, 0, sizeof s);
         for (int i = 0; i < n; i++)
         {
-            par[i] = i;
+            make(i + 1);
         }
         for (int i = 0; i < connections.size(); i++)
         {
             int a = connections[i][0];
             int b = connections[i][1];
-            Union(a,b);
+            Union(a + 1, b + 1);
         }
-        return connections.size()<n-1?-1:(n-1)-cc;
-        
+        return connections.size()<(n-1)?-1:(n-1)-cc;
     }
 };
