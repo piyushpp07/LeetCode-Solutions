045579e3ab97056eb4f7559cd2a2492bdf605a8a
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-11-11 10:04:30 -0800

    Added solution - LeetHub

--- IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
@@ -6,28 +6,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    vector<int> max_of_subarrays(vector<int> nums, int n, int k) {
-      deque<int>dq;
-    
-      int i = 0;
-      for(;i<k;i++)
-      {
-          while(dq.size()&&nums[i]>=nums[dq.back()])
-          dq.pop_back();
-          dq.push_back(i);
-      }
-      vector<int>ans;
-      for(;i<n;i++)
-      {
-        ans.push_back(nums[dq.front()]);
-        while(dq.size() && dq.front() <= i - k)
-            dq.pop_front();
-        while ((!dq.empty()) && nums[i] >= nums[dq.back()])
-            dq.pop_back();
-        dq.push_back(i); 
-      }
-      ans.push_back(nums[dq.front()]);
-      return ans;
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+            
+            deque<int>dq;
+            for(int i = 0;i<k;i++)
+            {
+                while(dq.size() && arr[dq.back()] <= arr[i])
+                dq.pop_back();
+                dq.push_back(i);
+            }
+            vector<int>v;
+            for(int i = k ;i<n;i++)
+            {
+                v.push_back(arr[dq.front()]);
+                while(dq.size() && dq.front() <= i - k)
+                dq.pop_front();
+                while(dq.size() && arr[dq.back()] <= arr[i])
+                dq.pop_back();
+                dq.push_back(i);
+            }
+            v.push_back(arr[dq.front()]);
+            
+            return v;
     }
 };
 
