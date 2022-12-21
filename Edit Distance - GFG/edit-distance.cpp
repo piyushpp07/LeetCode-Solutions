d31a44a40425a43b54b2866bbb1d95058064aa53
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2022-12-21 22:42:22 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -19,9 +19,28 @@ class Solution {
     }
     int editDistance(string s, string t) {
         int n = s.size(),m = t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(n-1,m-1,s,t,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 0;i<=m;i++)
+        {
+            dp[0][i] = i ;
+        }
+        for(int i = 0;i<=n;i++)
+        {
+            dp[i][0] = i; 
+        }
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+              if(s[i-1] == t[j-1])
+                dp[i][j] =  dp[i-1][j-1];
+             else
+              dp[i][j] =  1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+            }
+        }
+        return dp[n][m];
     }
+    
 };
 
 //{ Driver Code Starts.
