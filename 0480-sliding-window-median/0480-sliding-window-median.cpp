9c4aefbaf9a12241d0135c61064f10c1a687ae33
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2023-02-28 12:16:47 +0530

    Time: 239 ms (32.33%), Space: 15.8 MB (72.03%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -4,45 +4,39 @@ public:
     {
         double median;
         auto iter = data.cbegin();
-        advance(iter, n / 2);
-
-        // Middle or average of two middle values
-        if (n % 2 == 0)
+        advance(iter, n / 2);  // iterator pointing to middle of the window
+        if (n % 2 == 0) //if window if even sized then 
         {
             const auto iter2 = iter--;
-            median = double(*iter + *iter2) / 2;    // data[n/2 - 1] AND data[n/2]
+            median = double(*iter + *iter2) / 2;  
         }
-        else
+        else   // when window is odd size
         {
             median = *iter;
         }
         return median;
     }
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double>v,q;
-        int i =0,j =0,n = nums.size();
+        vector<double>v;  // ans array
+        int i =0,j =0,n = nums.size(); // sliding window variables 
         multiset<double>ms;
         for(;j<k;j++)
-            ms.insert(nums[j]);
-        auto it = ms.begin();
-        for(int p = 0;p<k/2;p++)
-            it++;
+            ms.insert(nums[j]);    // adding k elements to window 
         if(n == k)
-            return {median(ms,k)};
-        while(j<n)
+            return {median(ms,k)};   // if window == k we will return only one median
+        while(j<n)   // sliding window 
         {
-            if(j-i<k)
+            if(j-i<k)  // expanding till k
             {
                 ms.insert(nums[j]);
                 j++;
             }
-            if(j-i == k)
+            if(j-i == k)   //if window size is same as k then we will find median
             {
                 double d = median(ms,k);
                 v.push_back(d);
-                auto lb = ms.lower_bound(nums[i]);
+                auto lb = ms.lower_bound(nums[i++]); //shrinking window size
                 ms.erase(lb);
-                i++;
             }
         }
         return v;
