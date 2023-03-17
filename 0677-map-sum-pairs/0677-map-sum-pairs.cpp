400010687fb191ae012bb21f9709675f182cef9b
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2023-03-17 22:17:24 +0530

    Time: 7 ms (25.40%), Space: 8 MB (55.56%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.cpp
+++ 0677-map-sum-pairs/0677-map-sum-pairs.cpp
@@ -1,31 +1,57 @@
+class TrieNode{
+    public:
+    bool isCompleted;
+    TrieNode *character[26];
+    int curr;
+    TrieNode()
+    {   curr = 0;
+        isCompleted = false;
+        memset(character,0,sizeof character);
+    }
+};
 class MapSum {
 public:
-    unordered_map<string,int>mp;
+    TrieNode *root;
     MapSum() {
-        
+        root = new TrieNode();
     }
     
     void insert(string key, int val) {
-        mp[key] = val;
-    }
-    
-    int sum(string prefix) {
-        int cnt = 0;
-        for(auto i : mp)
+        TrieNode *node = root;
+        for(auto ch : key)
         {
-            string s = i.first;
-            if(prefix.size()<=s.size()){
-                bool ans = true;
-                for(int j = 0;j<prefix.size();j++)
-                {
-                    if(prefix[j]!=s[j])
-                        ans = false;
-                }
-                if(ans)
-                    cnt+=i.second;
-            }
+            int idx = ch - 'a';
+            if(node->character[idx] == NULL)
+                node->character[idx] = new TrieNode();
+            
+            node = node->character[idx];
+            
         }
-        return cnt;
+        node->curr = val;
+        node->isCompleted = true;
+    }
+    void helper(TrieNode *node,int &ans)
+    {
+        if(node->isCompleted)
+            ans += node->curr;
+        for(int i =0 ;i<26;i++)
+        {
+            if(node->character[i] != NULL)
+                helper(node->character[i],ans);
+        }
+    }
+    int sum(string prefix) {
+        int ans = 0;
+        TrieNode *node = root;
+        for(int i = 0;i<prefix.size();i++)
+        {
+            int idx = prefix[i] - 'a';
+            if(node->character[idx] == NULL)
+                return 0;
+            node = node->character[idx];
+        }
+        helper(node,ans);
+        return ans;
     }
 };
 
