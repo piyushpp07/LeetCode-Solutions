eb85efd273cdd537e9beb522461d04e132cd0c7c
zeus_007 <59327328+piyushpp07@users.noreply.github.com>
2023-05-29 10:30:33 +0530

    Time: 65 ms (49.23%), Space: 33 MB (60.01%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -0,0 +1,45 @@
+class ParkingSystem {
+public:
+    int big,medium,small;
+    ParkingSystem(int big, int medium, int small) {
+        this->big = big;
+        this->medium = medium;
+        this->small = small;
+    }
+    
+    bool addCar(int carType) {
+        if(carType == 3)
+        {
+            if(small == 0)
+                return false;
+            else{
+                small--;
+                return true;
+            }
+        }
+        else if(carType == 2)
+        {
+            if(medium == 0)
+                return false;
+            else{
+                medium--;
+                return true;
+            }
+        }
+        else
+        {
+            if(big == 0)
+                return false;
+            else{
+                big--;
+                return true;
+            }
+        }
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
